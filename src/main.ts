0fb38398c4d88ba10805c58b350a1ea641048029
rot1024 <aayhrot@gmail.com>
2022-06-09 20:38:11 +0900

    add url option

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ const version = getInput("version") || process.env.CHANGELOG_VERSION || "minor";
 const versionAsIs =
   getInput("versionAsIs") || process.env.CHANGELOG_VERSION_ASIS;
 const date = dateOrNow(getInput("date") || process.env.CHANGELOG_DATE);
+const url = dateOrNow(getInput("url") || process.env.CHANGELOG_URL);
 const latest = getInput("latest") || process.env.CHANGELOG_LATEST;
 const output =
   getInput("output") || process.env.CHANGELOG_OUTPUT || "CHANGELOG.md";
@@ -22,7 +23,10 @@ const output =
 
   const actualVersion =
     !versionAsIs && /^[0-9]/.test(version) ? `v${version}` : version;
-  const result = await exec(actualVersion, date, config);
+  const result = await exec(actualVersion, date, {
+    ...(config ?? {}),
+    url: url || config?.url,
+  });
   const newChangelog = insertChangelog(
     changelog || defaultChangelog,
     result.changelog,
