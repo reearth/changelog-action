0b416e1faa0cd89fe0c82785986bf1dc47fcbd29
rot1024 <aayhrot@gmail.com>
2022-07-01 14:13:31 +0900

    fix: move versionPrefix option to config

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,6 @@ const changelog_1 = require("./changelog");
 const githubAction = !!process.env.GITHUB_ACTIONS;
 const defaultChangelog = "# Changelog\n\nAll notable changes to this project will be documented in this file.";
 const version = (0, core_1.getInput)("version") || process.env.CHANGELOG_VERSION || "minor";
-const versionPrefix = (0, core_1.getInput)("versionPrefix") || process.env.CHANGELOG_VERSION_PREFIX;
-const titleVersionPrefix = (0, core_1.getInput)("titleVersionPrefix") || process.env.CHANGELOG_TITLE_VERSION_PREFIX;
 const date = dateOrNow((0, core_1.getInput)("date") || process.env.CHANGELOG_DATE);
 const repo = (0, core_1.getInput)("repo") || process.env.CHANGELOG_REPO;
 const latest = (0, core_1.getInput)("latest") || process.env.CHANGELOG_LATEST;
@@ -20,15 +18,15 @@ const noEmit = (0, core_1.getInput)("noEmit") || process.env.CHANGELOG_NO_EMIT;
 (async function () {
     const config = await loadJSON(configPath);
     const changelog = await load(output);
-    const actualVersion = versionPrefix === "add" && /^[0-9]/.test(version)
+    const actualVersion = config?.versionPrefix === "add" && /^[0-9]/.test(version)
         ? `v${version}`
-        : versionPrefix === "remove" && /^v[0-9]/.test(version)
+        : config?.versionPrefix === "remove" && /^v[0-9]/.test(version)
             ? version.slice(1)
             : version;
     const actualRepo = repo || config?.repo;
     const result = await (0, action_1.exec)(actualVersion, date, {
         ...(config ?? {}),
-        titleVersionPrefix: titleVersionPrefix || config?.titleVersionPrefix,
+        titleVersionPrefix: config?.titleVersionPrefix,
         repo: actualRepo === "false"
             ? undefined
             : actualRepo || process.env.GITHUB_REPOSITORY,
@@ -44,10 +42,10 @@ const noEmit = (0, core_1.getInput)("noEmit") || process.env.CHANGELOG_NO_EMIT;
     }
     if (!noEmit || noEmit !== "false") {
         await fs_1.promises.writeFile(output, newChangelog);
-        console.log(`${githubAction ? "\n" : ""}Changelog was saved: ${output}`);
+        console.log(`${githubAction ? "\n" : ""}Changelog was saved to ${output}`);
         if (latest) {
             await fs_1.promises.writeFile(latest, result.changelogWithoutTitle);
-            console.log(`Changelog only for the new version was saved: ${latest}`);
+            console.log(`Changelog only for the new version was saved to ${latest}`);
         }
     }
 })().catch((err) => {
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { promises } from "fs";
 
 import { getInput, setOutput, setFailed } from "@actions/core";
 
-import { exec } from "./action";
+import { exec, type Option } from "./action";
 import { insertChangelog } from "./changelog";
 
 const githubAction = !!process.env.GITHUB_ACTIONS;
@@ -10,10 +10,6 @@ const defaultChangelog =
   "# Changelog\n\nAll notable changes to this project will be documented in this file.";
 
 const version = getInput("version") || process.env.CHANGELOG_VERSION || "minor";
-const versionPrefix =
-  getInput("versionPrefix") || process.env.CHANGELOG_VERSION_PREFIX;
-const titleVersionPrefix =
-  getInput("titleVersionPrefix") || process.env.CHANGELOG_TITLE_VERSION_PREFIX;
 const date = dateOrNow(getInput("date") || process.env.CHANGELOG_DATE);
 const repo = getInput("repo") || process.env.CHANGELOG_REPO;
 const latest = getInput("latest") || process.env.CHANGELOG_LATEST;
@@ -25,21 +21,27 @@ const configPath =
   ".github/changelog.json";
 const noEmit = getInput("noEmit") || process.env.CHANGELOG_NO_EMIT;
 
+export type Config = Option & {
+  versionPrefix?: string;
+  titleVersionPrefix?: string;
+  defaultChangelog?: string;
+};
+
 (async function () {
-  const config = await loadJSON(configPath);
+  const config: Config | undefined = await loadJSON(configPath);
   const changelog = await load(output);
 
   const actualVersion =
-    versionPrefix === "add" && /^[0-9]/.test(version)
+    config?.versionPrefix === "add" && /^[0-9]/.test(version)
       ? `v${version}`
-      : versionPrefix === "remove" && /^v[0-9]/.test(version)
+      : config?.versionPrefix === "remove" && /^v[0-9]/.test(version)
       ? version.slice(1)
       : version;
   const actualRepo = repo || config?.repo;
 
   const result = await exec(actualVersion, date, {
     ...(config ?? {}),
-    titleVersionPrefix: titleVersionPrefix || config?.titleVersionPrefix,
+    titleVersionPrefix: config?.titleVersionPrefix,
     repo:
       actualRepo === "false"
         ? undefined
